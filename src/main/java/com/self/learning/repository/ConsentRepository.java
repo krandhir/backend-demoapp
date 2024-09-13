7fc7c633e7555183551afb5fa37384e765f74307
randhir kumar <22560544+krandhir@users.noreply.github.com>
2024-09-13 21:04:42 +0200

    updated datatype of primary key in repository

--- src/main/java/com/self/learning/repository/ConsentRepository.java
+++ src/main/java/com/self/learning/repository/ConsentRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConsentRepository extends JpaRepository<Consent, Long> {
+public interface ConsentRepository extends JpaRepository<Consent, String> {
 
 }
--- src/main/java/com/self/learning/repository/InstrumentRepository.java
+++ src/main/java/com/self/learning/repository/InstrumentRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InstrumentRepository extends JpaRepository<Instrument, Long> {
+public interface InstrumentRepository extends JpaRepository<Instrument, String> {
 
 }
