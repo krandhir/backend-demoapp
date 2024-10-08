630be6a49066ae1cb3217b1789574bb83da6c267
Randhir Kumar <22560544+krandhir@users.noreply.github.com>
2024-10-08 23:57:02 +0200

    Added service layer

--- src/main/java/com/self/learning/LearningApplication.java
+++ src/main/java/com/self/learning/LearningApplication.java
@@ -9,12 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningApplication {
 
-	/**
-	 * The entry point of application.
-	 *
-	 * @param args the input arguments
-	 */
-	public static void main(String[] args) {
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
 	}
 
